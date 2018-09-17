03ec28536b8bc9bd597941e971bbc8012fcc6e35
Dom O'Connor <dom@domoconnor.co.uk>
2018-09-17 10:50:11 +0100

    Fix exit codes and set environment

--- phpcs.py
+++ phpcs.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
+
 import subprocess, os, sys
 
 PHPCS_BIN = '/usr/local/bin/phpcs'
 PHPCS_STANDARD = 'PSR1,PSR2'
 PHPCS_SEVERITY = '6'
-PHPCS_IGNORE = '/app/Services/FinancialCalculationService.php'
+PHPCS_IGNORE = ''
 TMP = '.tmp_hooks'
 
 def runProcess(exe):    
@@ -11,13 +13,15 @@ def runProcess(exe):
     while(True):
       retcode = p.poll() #returns None while subprocess is running
       line = p.stdout.readline()
-      yield line
+      # Only return lines that are not entirely whitespace
+      line = line.rstrip()
+      if(line):
+        yield line
       if(retcode is not None):
         break
 
 p = subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE)
 gitDir = p.communicate()[0].rstrip()
-
 
 proc = subprocess.Popen(['git', 'diff', '--name-only', '--cached',
     '--diff-filter=ACMR'], stdout=subprocess.PIPE)
@@ -27,15 +31,19 @@ staged_files = [f.strip() for f in staged_files]
 staged_files = [gitDir+'/'+"".join(f) for f in staged_files]
 
 if(len(staged_files) == 0):
-   print('none')
+   print('You have no staged files')
    sys.exit()
 
-
-print 'started'
+errors = False
 for line in runProcess([
     PHPCS_BIN,
     '-q',
     '--standard='+PHPCS_STANDARD,
     '--warning-severity='+PHPCS_SEVERITY,
     '--ignore='+PHPCS_IGNORE] + staged_files):
-    print line,
+    # Print to stderr so we get the correct exit code
+    print line
+    errors = True
+
+# Exit with correct status code
+sys.exit(errors)
